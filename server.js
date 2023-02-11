96a934370eeceb27193a467b422b32298d68b197
timo9939 <timothylau20199@gmail.com>
2023-02-11 18:45:54 +0000

    server.js uploaded

---
+++ server.js
@@ -0,0 +1,26 @@
+const express= require('express')
+
+const mongodb = require('mongodb').MongoClient
+
+const app=express()
+const port =3001
+
+const connectionStringURI = `mongodb://127.0.0.1:27017/inventoryDB`;
+
+// Declare a variable to hold the connection
+let db;
+
+mongodb.connect(
+    // Defines connection between app and MongoDB instance
+    connectionStringURI,
+    // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      // Use client.db() constructor to add new db instance
+      db = client.db();
+      app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+      });
+    }
+  );
+  
