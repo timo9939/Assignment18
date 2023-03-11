dc42d39af783249a560d6866624d6032ecc9e1b4
timo9939 <timothylau20199@gmail.com>
2023-03-11 21:14:49 +0000

    update and delete User finished

--- server.js
+++ server.js
@@ -14,13 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", (req, res)=>{
+app.get("/readAll", (req, res)=>{
   User.find({})
   .then(data=>res.json(data))
   .catch(err=>res.status(500).json(err))
 })
 
-app.post("/add",(req,res)=>{
+app.get("/readOneUser/:_id", (req,res)=>{
+  User.findOne({
+    _id:req.params._id,
+  })
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
+app.post("/addUser",(req,res)=>{
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -28,6 +36,27 @@ app.post("/add",(req,res)=>{
   .then(data=>res.status(200).json(data))
   .catch(err=>res.status(500).json(err))
 })
+
+app.put("/User/update/:_id", (req,res)=>{
+  User.findOneAndUpdate(
+   
+    {_id:req.params._id},
+
+   req.body,
+    {new:true},
+  )
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
+app.delete("/User/delete/:_id",(req,res)=>{
+  User.deleteOne({
+    _id:req.params._id,
+  })
+  .then(data=>res.status(200).json(data))
+  .catch(err=>res.status(500).json(err))
+})
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
